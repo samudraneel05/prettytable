e6275e8e62fa73735f9786d584b717e0a814b0ff
Jan Stürtz <jan@bargten.de>
2025-01-31 10:04:33 +0100

    extend test for sortby as initialization param with some data

--- tests/test_prettytable.py
+++ tests/test_prettytable.py
@@ -804,7 +804,32 @@ class TestSorting:
         """
         Fix #354 where initialization of a table with sortby fails
         """
-        PrettyTable(field_names=["My", "Fields"], sortby="Fields")
+
+        table = PrettyTable(
+            field_names=["City name", "Area", "Population", "Annual Rainfall"],
+            sortby="Area",
+        )
+        table.add_row(["Adelaide", 1295, 1158259, 600.5])
+        table.add_row(["Brisbane", 5905, 1857594, 1146.4])
+        table.add_row(["Darwin", 112, 120900, 1714.7])
+        table.add_row(["Hobart", 1357, 205556, 619.5])
+        table.add_row(["Sydney", 2058, 4336374, 1214.8])
+        table.add_row(["Melbourne", 1566, 3806092, 646.9])
+        table.add_row(["Perth", 5386, 1554769, 869.4])
+        assert (
+            """+-----------+------+------------+-----------------+
+| City name | Area | Population | Annual Rainfall |
++-----------+------+------------+-----------------+
+|   Darwin  | 112  |   120900   |      1714.7     |
+|  Adelaide | 1295 |  1158259   |      600.5      |
+|   Hobart  | 1357 |   205556   |      619.5      |
+| Melbourne | 1566 |  3806092   |      646.9      |
+|   Sydney  | 2058 |  4336374   |      1214.8     |
+|   Perth   | 5386 |  1554769   |      869.4      |
+|  Brisbane | 5905 |  1857594   |      1146.4     |
++-----------+------+------------+-----------------+"""
+            == table.get_string().strip()
+        )
 
 
 @pytest.fixture(scope="function")
